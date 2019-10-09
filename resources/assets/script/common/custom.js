8dc8d23e4de134a98272d329fbe275dff950ffe7
Saleh Rabbi <33930388+salehrabbi@users.noreply.github.com>
2019-10-10 01:13:33 +0600

    all done witout mobile menu

---
+++ resources/assets/script/common/custom.js
@@ -0,0 +1,81 @@
+$(".puppies-slider").slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  prevArrow: '<img class="left" src="dist/images/Path.png" >',
+  nextArrow: '<img class="right" src="dist/images/Path.png" >',
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+        arrows: false
+      }
+    }
+  ]
+});
+
+$(".gif-images").slick({
+  dots: false,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 750,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        dots: true,
+        autoplay: true
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        autoplay: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+        autoplay: true
+      }
+    }
+  ]
+});
--- resources/assets/script/main.js
+++ resources/assets/script/main.js
@@ -1,3 +1,4 @@
 //@prepros-append common/jquery.js
 //@prepros-append common/bootstrap.min.js
+//@prepros-append common/slick.min.js
 //@prepros-append common/custom.js
